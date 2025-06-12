4e1995fcd2a6a6cef019e498545d3fa6b68f5a68
tuandomvn <tuandomvn@gmail.com>
2025-06-12 07:35:11 +0700

    add fullname

--- Acme.OnlineCourses/Students/Dtos/StudentDto.cs
+++ Acme.OnlineCourses/Students/Dtos/StudentDto.cs
@@ -22,6 +22,7 @@ public class StudentDto : AuditedEntityDto<Guid>
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string FullName { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public DateTime? DateOfBirth { get; set; }
