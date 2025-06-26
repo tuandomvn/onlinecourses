aeba1d5b17bfadc157e64c6f0103afde17283cb1
tuandomvn <tuandomvn@gmail.com>
2025-06-26 20:49:20 +0700

    insert

--- Acme.OnlineCourses/EmploymentSupport.cs
+++ Acme.OnlineCourses/EmploymentSupport.cs
@@ -3,14 +3,14 @@ using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Acme.OnlineCourses
 {
-    public class EmploymentSupport : FullAuditedEntity<Guid>
+    public class EmploymentSupport : Volo.Abp.Domain.Entities.Auditing.FullAuditedEntity<System.Guid>
     {
         public string FullName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
-        public DateTime CourseCompletionDate { get; set; }
+        public System.DateTime CourseCompletionDate { get; set; }
         public string Message { get; set; }
     }
 } 
