6b1afe395efd0c82d41709dfe8441945dd869064
Hien.Phan <phanvuminhhien1204@gmail.com>
2025-06-26 21:43:01 +0700

    add Employment Support page and related files

---
+++ Acme.OnlineCourses/Pages/EmploymentSupport/EmploymentSupport.cshtml.cs
@@ -0,0 +1,11 @@
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace Acme.OnlineCourses.Pages.EmploymentSupport;
+
+public class EmploymentSupportModel : AbpPageModel
+{
+    public async Task OnGetAsync()
+    {
+        await Task.CompletedTask;
+    }
+}
