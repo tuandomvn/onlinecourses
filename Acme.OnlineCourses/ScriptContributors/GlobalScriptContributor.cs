fa854edac2290e402aac07bcf8e72ee67056d737
Hien.Phan <phanvuminhhien1204@gmail.com>
2025-06-28 09:40:54 +0700

    Update navigation and mobile menu functionality

--- Acme.OnlineCourses/ScriptContributors/GlobalScriptContributor.cs
+++ Acme.OnlineCourses/ScriptContributors/GlobalScriptContributor.cs
@@ -8,6 +8,7 @@ namespace Acme.OnlineCourses.ScriptContributors
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
             context.Files.AddIfNotContains("/js/animate.js");
+            //context.Files.AddIfNotContains("/js/site.js");
         }
     }
 }
