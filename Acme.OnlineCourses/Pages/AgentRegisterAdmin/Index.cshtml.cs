49d2831cc6fb73d991c76a9802876acc8e1472f3
tuandomvn <tuandomvn@gmail.com>
2025-06-29 17:08:50 +0700

    update

--- Acme.OnlineCourses/Pages/AgentRegisterAdmin/Index.cshtml.cs
+++ Acme.OnlineCourses/Pages/AgentRegisterAdmin/Index.cshtml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Volo.Abp.Domain.Repositories;
 using Acme.OnlineCourses;
+using System.Linq;
 
 namespace Acme.OnlineCourses.Pages.AgentRegisterAdmin
 {
@@ -17,10 +19,25 @@ namespace Acme.OnlineCourses.Pages.AgentRegisterAdmin
         }
 
         public IList<AgentRegister> AgentRegisters { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public int CurrentPage { get; set; } = 1;
+        
+        public int PageSize { get; set; } = 10;
+        
+        public int TotalCount { get; set; }
+        
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
 
         public async Task OnGetAsync()
         {
-            AgentRegisters = await _agentRegisterRepository.GetListAsync();
+            TotalCount = (int)await _agentRegisterRepository.GetCountAsync();
+            
+            AgentRegisters = await _agentRegisterRepository.GetPagedListAsync(
+                skipCount: (CurrentPage - 1) * PageSize,
+                maxResultCount: PageSize,
+                sorting: "CreationTime DESC"
+            );
         }
     }
 } 
--- Acme.OnlineCourses/Pages/EmploymentSupportAdmin/Index.cshtml.cs
+++ Acme.OnlineCourses/Pages/EmploymentSupportAdmin/Index.cshtml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Volo.Abp.Domain.Repositories;
 using Acme.OnlineCourses;
+using System.Linq;
 
 namespace Acme.OnlineCourses.Pages.EmploymentSupportAdmin
 {
@@ -17,10 +19,25 @@ namespace Acme.OnlineCourses.Pages.EmploymentSupportAdmin
         }
 
         public IList<EmploymentSupport> EmploymentSupports { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public int CurrentPage { get; set; } = 1;
+        
+        public int PageSize { get; set; } = 10;
+        
+        public int TotalCount { get; set; }
+        
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
 
         public async Task OnGetAsync()
         {
-            EmploymentSupports = await _employmentSupportRepository.GetListAsync();
+            TotalCount = (int)await _employmentSupportRepository.GetCountAsync();
+            
+            EmploymentSupports = await _employmentSupportRepository.GetPagedListAsync(
+                skipCount: (CurrentPage - 1) * PageSize,
+                maxResultCount: PageSize,
+                sorting: "CreationTime DESC"
+            );
         }
     }
 } 
